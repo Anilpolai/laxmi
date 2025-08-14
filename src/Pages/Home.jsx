9bddedd1af974c8c64812819ba93ad97b83cb484
preneel <anil.polai03@gmail.com>
2025-08-14 05:35:42 +0530

    add the shop

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Marktag from '../component/marketag/marktag'
 import CategorySection from '../component/categories/categories'
 import NewArrival from '../component/newarrival/newarrival'
 import BestSellers from '../component/newseller/bestselling'
+import ShopSwiper from '../component/shoplook/shopswiper'
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <CategorySection />
       <BestSellers/>
       <NewArrival />
+      <ShopSwiper/>
     </>
   )
 }
---
+++ src/component/shoplook/shopswiper.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./ShopSwiper.css";
+
+// Custom Arrow Components
+function NextArrow({ onClick }) {
+  return (
+    <div className="custom-arrow next" onClick={onClick}>
+      &#8250;
+    </div>
+  );
+}
+
+function PrevArrow({ onClick }) {
+  return (
+    <div className="custom-arrow prev" onClick={onClick}>
+      &#8249;
+    </div>
+  );
+}
+
+export default function ShopSwiper() {
+  const products = [
+    {
+      id: 1,
+      img: "https://www.anantexports.in/cdn/shop/files/IMG-20240315-WA0054.jpg?v=1710444561",
+      name: "Layla Smock Dress in Black Linen",
+      price: "$300.00",
+      oldPrice: "$350.00",
+    },
+    {
+      id: 2,
+      img: "https://www.anantexports.in/cdn/shop/files/IMG-20240315-WA0054.jpg?v=1710444561",
+      name: "Calissa Midi Dress in Navy Linen",
+      price: "$250.00",
+    },
+    {
+      id: 3,
+      img: "https://www.anantexports.in/cdn/shop/files/IMG-20240315-WA0054.jpg?v=1710444561",
+      name: "Wear Sequins Embroidered Navy",
+      price: "$245.00",
+    },
+    {
+      id: 4,
+      img: "https://www.anantexports.in/cdn/shop/files/IMG-20240315-WA0054.jpg?v=1710444561",
+      name: "Violet Maxi Dress in Navy Spot",
+      price: "$150.00",
+    },
+    {
+      id: 5,
+      img: "https://www.anantexports.in/cdn/shop/files/IMG-20240315-WA0054.jpg?v=1710444561",
+      name: "Paris Dream Dress - Gold Aqua",
+      price: "$240.00",
+    },
+  ];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      { breakpoint: 1200, settings: { slidesToShow: 3 } },
+      { breakpoint: 768, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1 } },
+    ],
+  };
+
+  return (
+    <div className="shop-swiper-section">
+      <div className="shop-swiper-heading">
+        <h2>Shop the Look</h2>
+        <p>Unmatched design — superior performance and customer satisfaction in one.</p>
+      </div>
+
+      <Slider {...settings}>
+        {products.map((item) => (
+          <div key={item.id} className="shop-slide">
+            <div className="shop-slide-inner">
+              <img className="shop-main-img" src={item.img} alt={item.name} />
+              <div className="shop-card-overlay">
+                <img className="shop-thumb" src={item.img} alt="mini" />
+                <div className="shop-card-info">
+                  <div className="shop-card-title">{item.name}</div>
+                  <div className="shop-card-price">
+                    {item.price}
+                    {item.oldPrice && (
+                      <span className="shop-card-oldprice">{item.oldPrice}</span>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
